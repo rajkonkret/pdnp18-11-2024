2d24b98401a3d17fdd9e93d6a512c6163e2ba51b
rjaniak <rajkonkret660@gmail.com>
2024-11-18 14:36:09 +0100

    [1 2] add pierwszy.py

---
+++ day1/pierwszy.py
@@ -0,0 +1,15 @@
+print()
+# Process finished with exit code 0 - program zakończony poprawnie
+
+print("Nazywam się Radek")
+print('Nazywam się Radek')
+print('Nazywam się Radek')
+# ctrl d - powielanie linijki
+# Nazywam się Radek
+# Nazywam się Radek
+# ctrl / - automatyczny komentarz
+# ctrl alt l - formatowanie
+
+# sprawdzenie jakiego typu to są dane
+# type()
+print(type("Radek"))  # <class 'str'>, string, typ tekstowy
