bb06c7fd44912c2547545a10445a489cd35cda93
rjaniak <rajkonkret660@gmail.com>
2024-11-18 15:45:24 +0100

    [1 5] update teksty.py

--- day1/teksty.py
+++ day1/teksty.py
@@ -9,8 +9,28 @@ tekst.upper()
 """ Return a copy of the string converted to uppercase. """
 print(tekst)
 print(tekst.upper())  # WITAJ ŚWIECIE
-tekst_upper = tekst.upper() # do zmiennej przechwytujemy zmieniony tekst
+tekst_upper = tekst.upper()  # do zmiennej przechwytujemy zmieniony tekst
 print(tekst_upper)  # WITAJ ŚWIECIE
 
 tekst_lower = tekst.lower()
 print(tekst_lower)  # witaj świecie
+
+# Witaj Świecie
+# 0123456789012
+print(tekst.count("i"))  # występuje 3 razy, sprawdza cały tekst
+print(tekst.count("j", 0, 4))  # występuje 0 razy, z prawej zbiór otwarty, indeksy 0123
+print(tekst.count("j", 0, 4))
+# tego x: __start __end nie piszemy. To tylko podpowiedź
+# Wita
+# 0123
+
+print(tekst.removesuffix("Świecie"))  # "Witaj "
+print(tekst.removeprefix("Witaj"))  # " Świecie"
+
+tekst_zamiana = "Witaj dobry Świecie"
+print(tekst_zamiana.replace("dobry", " "))  # Witaj   Świecie
+print(tekst_zamiana.replace("dobry ", " "))  # Witaj  Świecie
+
+# strip() - usunięcie białych znków i spacji na początku i końcu tekstu
+print(tekst.removeprefix("Witaj").strip())  # "Świecie"
+print(tekst.removesuffix("Świecie").strip())  # "Witaj"
