2f18539309ae0152125833af723ba740dd4246fa
rjaniak <rajkonkret660@gmail.com>
2024-11-21 13:40:55 +0100

    [4 11] update fun4

--- day4/fun_4.py
+++ day4/fun_4.py
@@ -1,6 +1,6 @@
 # stworzyc funkcję obliczającą średnią
 
-def srednia(*cyfry):  # *cyfry , dowolna ilośc argumentów po pozycji 1,2,3,4,5...
+def srednia(name=None, *cyfry):  # *cyfry , dowolna ilośc argumentów po pozycji 1,2,3,4,5...
     print(cyfry)  # ()
     count = len(cyfry)
     suma = 0
@@ -11,9 +11,11 @@ def srednia(*cyfry):  # *cyfry , dowolna ilośc argumentów po pozycji 1,2,3,4,5
     except Exception as e:
         print("Bład", e)
     else:
-        print(f"Średnia awynosi {avg}")
+        print(f"Średnia dla ucznia {name} wynosi {avg}")
     finally:
         print("Kolejny uczeń")
+
+
 # ()
 # Bład division by zero
 # Kolejny uczeń
@@ -35,3 +37,20 @@ srednia(1, 2, 3, 4, 5, 6, 7, 8, 9)
 # (1, 2)
 # (4, 5, 6, 5, 6)
 # (1, 2, 3, 4, 5, 6, 7, 8, 9)
+srednia("Radek", 4, 5, 6, 5, 6, 4)
+# name, *cyfry = "Radek", 4, 5, 6, 5, 6, 4
+# ()
+# Bład division by zero
+# Kolejny uczeń
+# (2,)
+# Średnia dla ucznia 1 wynosi 2.0
+# Kolejny uczeń
+# (5, 6, 5, 6)
+# Średnia dla ucznia 4 wynosi 5.5
+# Kolejny uczeń
+# (2, 3, 4, 5, 6, 7, 8, 9)
+# Średnia dla ucznia 1 wynosi 5.5
+# Kolejny uczeń
+# (4, 5, 6, 5, 6, 4)
+# Średnia dla ucznia Radek wynosi 5.0
+# Kolejny uczeń
